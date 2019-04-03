9af56870cb83531301990d4b4ca25509c63739b0
NeilP <neilgbpate1@gmail.com>
2019-04-02 21:15:52 -0400

    Initial Commit

---
+++ app.js
@@ -0,0 +1,39 @@
+// @TODO: YOUR CODE HERE!
+// Step 1: Set up our chart
+//= ================================
+var svgWidth = 960;
+var svgHeight = 500;
+
+var margin = {
+    top: 20,
+    right: 40,
+    bottom: 60,
+    left: 50
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+// Step 2: Create an SVG wrapper,
+// append an SVG group that will hold our chart,
+// and shift the latter by left and top margins.
+// =================================
+var svg = d3
+    .select("body")
+    .append("svg")
+    .attr("width", svgWidth)
+    .attr("height", svgHeight);
+
+var chartGroup = svg.append("g")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+// Step 3:
+// Import data from the donuts.csv file
+// =================================
+d3.csv("data.csv", function (error, Data) {
+    if (error) throw error;
+
+  // Step 4: Parse the data
+  // Format the data and convert to numerical and date values
+  // =================================
+  // Create a function to parse date and time
